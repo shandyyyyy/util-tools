621b6d1fcdc427a63a036de3d4638eac60b60fff
huhan <huhan@ebay.com>
2021-08-02 17:13:05 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development'
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  }
 }
